52ef4a05e5dd909a5a9e373b5da1441801496973
csumphan <csumphan@uci.edu>
2019-03-05 17:43:08 -0800

    Updates

---
+++ precache-manifest.4bbe5284d9d0dac2bb195daddb52eaa1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7feaed1ce306d6d7c32",
+    "url": "/AirpodProject/static/css/main.98f09e1d.chunk.css"
+  },
+  {
+    "revision": "b7feaed1ce306d6d7c32",
+    "url": "/AirpodProject/static/js/main.b7feaed1.chunk.js"
+  },
+  {
+    "revision": "49675a82a1568e07345d",
+    "url": "/AirpodProject/static/js/runtime~main.49675a82.js"
+  },
+  {
+    "revision": "ce581c05ad05b85747cb",
+    "url": "/AirpodProject/static/js/2.ce581c05.chunk.js"
+  },
+  {
+    "revision": "83c5e1f54b75c39061483eb67c796ce0",
+    "url": "/AirpodProject/static/media/AirPods_Vector.83c5e1f5.svg"
+  },
+  {
+    "revision": "cbfac30a81e43aa8c0f00d25d759e6a3",
+    "url": "/AirpodProject/static/media/honne.cbfac30a.mp3"
+  },
+  {
+    "revision": "7fcc96135b001580c4f3963c9382bccb",
+    "url": "/AirpodProject/static/media/walking.7fcc9613.mp3"
+  },
+  {
+    "revision": "50acb6389e6a646dba415b16996a7c43",
+    "url": "/AirpodProject/static/media/person.50acb638.png"
+  },
+  {
+    "revision": "2e7e33c0643413b70205807cce51fea0",
+    "url": "/AirpodProject/index.html"
+  }
+];
